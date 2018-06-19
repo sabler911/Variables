8af15a6fa521b34c20a4e5a7922c5c3c099bafe5
sabler911 <mr_wolf20011@yahoo.com>
2018-06-18 23:44:32 -0400

    First Comment
    Uploading All files

---
+++ Variables/src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+	
+	public static void main(String[] args) {
+		int myNumber = 99;
+		short myShort = 854;
+		long myLong = 9865;
+		
+		double myDouble = 8.6548;
+		float myFloat = 324.4f;
+		
+		char myChar = 'y';
+		boolean myBoolean = false;
+		
+		byte myByte = 127;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
